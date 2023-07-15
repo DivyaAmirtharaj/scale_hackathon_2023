15bfebdafcc09c64cfe656a39a3308f6d918abea
Jothi Ramaswamy <jothiramaswamy@thinskteam4girls.org>
2023-07-15 16:15:56 -0700

    WIP

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
+import threading
+import time
 
 app = Flask(__name__)
+
+global_queue = []
 
 @app.route('/kaboom', methods=['POST'])
 def post_data():
@@ -9,9 +13,18 @@ def post_data():
     text = request.form.get('text')
     user_id = request.form.get('user_id')
 
-    data = "\n".join([channel_id, text, user_id])
+    data = [channel_id, text, user_id]
+    global_queue.append(data)
 
     # Do something with the data here
 
     # Send a response
-    return jsonify({"status": "success", "data": data}), 200
+    return jsonify({"status": "success", "data": "\n".join(data)}), 200
+
+def monitor_list():
+    while True:
+        if global_queue:
+            print("List is populated: ", global_queue)
+        else:
+            print("List is empty")
+        time.sleep(1)  # Pause for a second between checks
