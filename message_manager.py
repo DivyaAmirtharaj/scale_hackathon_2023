9bf013669ae43e22be3d78462f3405ac6a53543c
Divya <divya.amirtharaj@gmail.com>
2023-07-15 17:10:46 -0700

    cleaned up structure

--- message_manager.py
+++ message_manager.py
@@ -9,17 +9,19 @@ class MessageManager():
         self.database.delete_table()
         self.database.create_table()
         
-        self.slack_api_manager = SlackAPIManager('xoxb-5571078244199-5571084859927-Q6uxPD2B9SfiZW8ceQChd5qy')
+        # Get data from Slack
+        self.slack_api_manager = SlackAPIManager('xoxb-5571078244199-5571084859927-QzeaiU7RY6OGGYVk7JUrj96S')
         user_json = self.slack_api_manager.get_user_list()
         messages_json = self.slack_api_manager.get_conversation_history('C05H7ET044T')
 
+        
+        # Populate users table
         user_dict = clean_user_list(user_json)
         for key, value in user_dict:
             character_prompt = ""
             self.database.add_users(key, value["name"], character_prompt)
 
         # Populate conversation/ messages table
-        messages_json = self.slack_api_manager.get_conversation_history('C05H7ET044T')
         message_dict, conversation_dict = clean_message_list(messages_json)
         for key, value in conversation_dict:
             self.database.add_conversation(key, value)
